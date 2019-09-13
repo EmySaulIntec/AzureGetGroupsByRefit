cd60550aed38a0e24098cbeda06b2b8766255bf0
emysaulintec <emysaulintec@gmail.com>
2019-09-13 13:37:46 -0400

    removed key

--- Azure/Azure/Azure/Utils/Config.cs
+++ Azure/Azure/Azure/Utils/Config.cs
@@ -9,7 +9,7 @@ namespace Azure.Utils
     {
         public const string NameAzureKey = "Ocp-Apim-Subscription-Key";
 
-        public const string ApiKey = "06e5b9acb8064452b892004cc25fdfab";
+        public const string ApiKey = "";
 
         public const string HostUri = "https://eastus2.api.cognitive.microsoft.com/face/v1.0";
 
